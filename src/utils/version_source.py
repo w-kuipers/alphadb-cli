cdc856f723bda59264168171193d3b01e880f20f
w-kuipers <wibokuip@gmail.com>
2024-01-04 13:02:56 +0000

    Remove all non mysql engine stuff

--- src/utils/version_source.py
+++ src/utils/version_source.py
@@ -13,72 +13,88 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 import json
-from src.utils.config import config_get, config_write
-from requests import get
-from requests.exceptions import JSONDecodeError, ConnectionError
+import os
+
 from inquirer import Text, prompt
 from inquirer.errors import ValidationError
-from src.utils.common import console
+from requests import get
+from requests.exceptions import ConnectionError, JSONDecodeError
 
-"Adding a new version source to config"
+from src.utils.common import console
+from src.utils.config import config_get, config_write
+
+
 def add_version_source() -> str | None:
-    ## ! Do not allow spaces
-    print("Version sources can either be local JSON files or URL's returning JSON data.\n")
-    
-    try: ## To catch ConnectionError
-        #### Function to validate path/url
+    "Adding a new version source to config"
+
+    print(
+        "Version sources can either be local JSON files or URL's returning JSON data.\n"
+    )
+
+    try:  ## To catch ConnectionError
+
         def pathval(path: str):
             if path[0:4] == "http":
                 r = get(path)
                 if not r.status_code == 200:
-                    raise ValidationError("", reason=f"URL responded with {r.status_code}")
+                    raise ValidationError(
+                        "", reason=f"URL responded with {r.status_code}"
+                    )
                 return True
 
             if not os.path.isfile(path):
-                raise ValidationError("", reason="This path does not point towards a JSON file.")
+                raise ValidationError(
+                    "", reason="This path does not point towards a JSON file."
+                )
             return True
-    
-        #### Promt user to input path
-        questions = [Text("path", message="Please provide either an absolute local file path or a local/remote URL", validate=lambda _, x: pathval(x))]
+
+        questions = [
+            Text(
+                "path",
+                message="Please provide either an absolute local file path or a local/remote URL",
+                validate=lambda _, x: pathval(x),
+            )
+        ]
         answers = prompt(questions)
-        
+
         #### If answers is None, user probably aborted
-        if answers == None: return
+        if answers == None:
+            return
 
         path = answers["path"]
-        
+
         if path[0:4] == "http":
             template_name = get(path).json()["name"]
         else:
             with open(path) as version_json:
                 template_name = json.load(version_json)["name"]
 
-    #### TODO This connection error catch does not seem to work  
+    #### TODO This connection error catch does not seem to work
     except ConnectionError:
         console.print("\n[red]Unable to establish connection[/red]\n")
         return
 
     except JSONDecodeError:
-        console.print("\n[red]The supplied URL did not respond with compatible data[/red]\n")
+        console.print(
+            "\n[red]The supplied URL did not respond with compatible data[/red]\n"
+        )
         return
 
     ##### TODO maybe add a check if the data has no errors
-    
+
     questions = [Text("name", message="Name the template", default=template_name)]
     answers = prompt(questions)
 
-    #### If answers is NOne, user probably aborted
-    if answers == None: return
+    #### If answers is None, user probably aborted
+    if answers == None:
+        return
 
     name = answers["name"]
 
-    ##http://localhost:8000/fmm/v1/database/db-version-config-information/
-
-    #### Write to config file
     vs = config_get("VERSION_SOURCES", fallback=True)
-    if vs == None: vs = {}
+    if vs == None:
+        vs = {}
     vs[name] = path
     config_write({"VERSION_SOURCES": vs})
 
